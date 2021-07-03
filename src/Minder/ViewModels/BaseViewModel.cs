0748625ae128b4c2937f9ce7f44b97c10509d550
kostyaLem <kostyaLem@yandex.ru>
2021-07-04 01:54:09 +0300

    added core projects

---
+++ src/Minder/ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace Minder.ViewModels
+{
+    /// <summary>
+    /// Базовая ViewModel для всех вью моделей
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        // Иконка модели
+        public abstract ImageSource PageIcon { get; }
+
+        // Выделена ли вкладка
+        public virtual bool IsSelected { get; set; }
+
+        // Заголовок вкладки
+        public abstract string PageTitle { get; }
+
+        // Событие уведомления об изменении свойств
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged([CallerMemberName] string param = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(param));
+        }
+    }
+}
