65fdfde734b4ccd588149c71b7f71cc745c10023
kostyaLem <kostyaLem@yandex.ru>
2022-03-18 02:10:16 +0300

    feat: add service for theme management

---
+++ src/Minder/Helpers/Services/ThemeService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace Minder.Helpers.Services
+{
+    public enum SkinType
+    {
+        Default,
+        Dark,
+    }
+
+    public static class ThemeService
+    {
+        public static void SetTheme(SkinType skinType)
+        {
+            var themeDictionary = App.Current.Resources.MergedDictionaries[0];
+            themeDictionary.Clear();
+
+            themeDictionary.MergedDictionaries.Add(GetSkin(skinType));
+            themeDictionary.MergedDictionaries.Add(new ResourceDictionary
+            {
+                Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+            });
+
+            App.Current.MainWindow.OnApplyTemplate();
+        }
+
+        private static ResourceDictionary GetSkin(SkinType skin) => new()
+        {
+            Source = new Uri($"pack://application:,,,/HandyControl;component/Themes/Skin{skin}.xaml")
+        };
+    }
+}
